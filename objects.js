c637eb9cd04ea1c09c1f475b8b0ab5c61ec85b4e
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-10-16 11:14:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,14 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 
-// playlist['Slowdive'] = 'Alison'
-// playlist['My Bloody Valentine'] = 'Sometimes'
-
-// function updatePlaylist(){
-//  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
-//  return playlist
-// }
-
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
   return playlist
